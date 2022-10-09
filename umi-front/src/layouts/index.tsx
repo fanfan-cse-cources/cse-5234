df747590e499e2d3964bb97765b26164571c77c1
Yifan Yao <fanfannnmn@protonmail.ch>
2022-10-09 14:34:49 -0400

    Add logo

--- umi-front/src/layouts/index.tsx
+++ umi-front/src/layouts/index.tsx
@@ -1,31 +1,32 @@
 import { Link, Outlet } from 'umi';
 import styles from './index.less';
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 export default function Layout() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Dragon's Wok</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">About US</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/purchase">Purchase</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/purchase/viewOrder">View Orders</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <>
+      <Navbar bg="light" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={require('../assets/logo.png')}
+              width="35"
+              height="35"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/purchase">Purchase</Nav.Link>
+              <Nav.Link href="/purchase/viewOrder">View Orders</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Outlet />
-    </div>
-
+    </>
   );
 }
