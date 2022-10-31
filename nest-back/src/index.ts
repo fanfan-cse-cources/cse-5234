8902022e37851b0b155e27eef2cc617673ef3bf6
frostwing98 <498924217@qq.com>
2022-10-30 20:29:46 -0400

    The page redirection with parameters

--- nest-back/src/index.ts
+++ nest-back/src/index.ts
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   entities: [Item, Order, PaymentInfo, AddressInfo],
-  synchronize: true,
+  // synchronize: true,
   logging: true,
 });
 
